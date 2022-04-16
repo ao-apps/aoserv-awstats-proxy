a8e0662d40568e08c1f111e78528165386f7ef5a
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:00:23 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoindustries/awstats_proxy/AWStatsProxy.java
+++ src/main/java/com/aoindustries/awstats_proxy/AWStatsProxy.java
@@ -1,6 +1,6 @@
 /*
  * aoserv-awstats-proxy - Webapp that publishes AWStats reports from the AOServ Platform.
- * Copyright (C) 2006-2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2006-2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with aoserv-awstats-proxy.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoindustries.awstats_proxy;
 
 import com.aoapps.hodgepodge.io.AOPool;
